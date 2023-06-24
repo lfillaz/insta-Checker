3f82aa048fc03969fd8fb8eb38c5115c41bad4d3
Lfil laz <114345508+lfillaz@users.noreply.github.com>
2023-06-25 00:53:16 +0300

    Update pip.py

--- pip.py
+++ pip.py
@@ -1,21 +1,6 @@
-import subprocess
-
-libraries = [
-    "user_agent",
-    "datetime",
-    "requests",
-    "queue",
-    "pyfiglet",
-    "art"
-]
-
-def install_libraries():
-    for library in libraries:
-        try:
-            print(f"Installing {library}...")
-            subprocess.check_call([sys.executable, "-m", "pip", "install", library])
-            print(f"{library} installed successfully!")
-        except subprocess.CalledProcessError:
-            print(f"Failed to install {library}!")
-
-install_libraries()
+pip install user_agent
+pip install datetime
+pip install requests
+pip install queue
+pip install pyfiglet
+pip install art
